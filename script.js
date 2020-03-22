8a964f4e3638fc1c8f2548993e257e4b97d0ccc6
dms2303 <55004787+dms2303@users.noreply.github.com>
2020-03-22 12:33:49 -0400

    Updated form, v-modeling, script added

---
+++ script.js
@@ -0,0 +1,17 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    form: [{
+      fullname: "",
+      email: "",
+      selfie: "",
+      headline: "",
+      title: "",
+      summary: "",
+      website: "",
+      checkedSkills: [],
+    }],
+  },
+    ]
+  }
+});
